b782dfa4b8014ba25fca521c46672e436385f8ab
Nayeem Nishaat <47172193+NayeemNishaat@users.noreply.github.com>
2024-02-19 00:10:10 +0600

    implemented sum rpc

--- client/client.js
+++ client/client.js
@@ -3,13 +3,21 @@ const protoLoader = require("@grpc/proto-loader");
 const packageDefinition = protoLoader.loadSync("../protos/greet.proto");
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 const greeterService = protoDescriptor.greet.GreetService;
+const sumService = grpc.loadPackageDefinition(
+  protoLoader.loadSync("../protos/sum.proto", {})
+).sum.SumService;
 
-const client = new greeterService(
+const greetClient = new greeterService(
   "localhost:50051",
   grpc.credentials.createInsecure()
 );
 
-client.Greet(
+const sumClient = new sumService(
+  "localhost:50051",
+  grpc.credentials.createInsecure()
+);
+
+greetClient.Greet(
   { greeting: { firstName: "Nayeem", lastName: "Nishaat" } },
   (error, response) => {
     if (!error) {
@@ -19,3 +27,11 @@ client.Greet(
     }
   }
 );
+
+sumClient.Sum({ num1: 10, num2: 5 }, (error, response) => {
+  if (!error) {
+    console.log(response.total);
+  } else {
+    console.error(error);
+  }
+});
--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const protoLoader = require("@grpc/proto-loader");
 const packageDefinition = protoLoader.loadSync("../protos/greet.proto", {});
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 const greetService = protoDescriptor.greet.GreetService;
+const sumService = grpc.loadPackageDefinition(
+  protoLoader.loadSync("../protos/sum.proto", {})
+).sum.SumService;
 
 /**
  * Implemen Greet RPC method
@@ -11,8 +14,16 @@ function greet(call, callback) {
   callback(null, { result: call.request.greeting.firstName });
 }
 
+/**
+ * Sum RPC method
+ */
+function sum(call, callback) {
+  callback(null, { total: call.request.num1 + call.request.num2 });
+}
+
 const server = new grpc.Server();
 server.addService(greetService.service, { Greet: greet });
+server.addService(sumService.service, { Sum: sum });
 
 server.bindAsync(
   "0.0.0.0:50051",
